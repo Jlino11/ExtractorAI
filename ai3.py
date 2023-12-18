f273f429e4f0560c7062f69ebe06881fcd89a365
Jlino11 <97848235+Jlino11@users.noreply.github.com>
2023-12-17 19:08:21 -0600

    Update ai3 with notes
    In this update the code generate a document with promts that you would use investigate the NVT

--- ai3.py
+++ ai3.py
@@ -1,9 +1,23 @@
 import re
 
+# Nombre del archivo de salida
+output_file = input("Nombre del archivo de salida: ")
+# Nombre del archivo axuiliar para los promts
+ai_info = input("Nombre del archivo para los promts: ")
+# Instriccion para la IA
+promt = " en espanol describe en 3 puntos lo siguiente: \nLa vulnerabilidad consiste en \nUna explotacion exitosa \nLa mitigacion sugerida es"
+# Mensaje de vulnerabilidad duplicada
+mensaje = "Dicha vulnerabilidad ya fue descrita con anterioridad."
+# Resumen del Host
+host_resum = "A continuacion la siguiente imagen muestra un resumen de los puertos afectados del host en particular."
+# Introduccion de la vulnerabilidad
+intro_nvt = "La siguiente vulnerabilidad es denominada "
 # Abre el archivo de texto
 with open("summary.txt", "r") as f:
     # Crea un diccionario vacío para almacenar las relaciones
     hosts = {}
+    # Crea un lista para NVT unicos
+    uniq_nvt = []
     # Crea una variable para almacenar la dirección IP actual
     current_host = None
     # Itera sobre cada línea del archivo
@@ -19,9 +33,15 @@ with open("summary.txt", "r") as f:
         # Busca una línea que contenga "NVT" en la línea actual
         match = re.search(r"NVT:", line)
         if match:
+            temp_nvt = line
             # Si se encuentra una línea que contenga "NVT", agrega la línea al diccionario de hosts para la dirección IP actual
             if line.strip() not in hosts[current_host]:
                 hosts[current_host].append(line.strip())
+            # Revisa si es un NVT unico
+            if line.strip() not in uniq_nvt:
+                text = line.strip() + promt
+                uniq_nvt.append(text)
+                
 
     # Crea un diccionario vacío para almacenar las relaciones inversas (NVT a host)
     nvts = {}
@@ -36,14 +56,31 @@ with open("summary.txt", "r") as f:
                 nvts[nvt].append(host)
 
     # Crea un archivo de texto para almacenar los resultados
-    with open("resultados.txt", "w") as f:
+    with open(output_file, "w") as f:
         # Imprime las relaciones de host a NVT y NVT a host para cada dirección IP válida en el archivo de texto
         for host in hosts:
             if len(hosts[host]) > 0:
                 f.write(f"Host {host}\n")
-                for nvt in hosts[host]:
-                    f.write(nvt + "\n")
-                    f.write(str([h for h in nvts[nvt] if h != host]) + "\n")
-                    f.write("\n")
+                f.write("\n")
+                f.write(f"{host_resum}\n")
+                f.write("\n")
+                f.write("\n")
 
-    print("Los resultados se han guardado en el archivo 'resultados.txt'.")
+                for nvt in hosts[host]:
+                    if len(nvts[nvt]) > 0:
+                        if nvts[nvt][0] == host:
+                            f.write(intro_nvt + nvt + "\n")
+                            f.write(str([h for h in nvts[nvt] if h != host]) + "\n")
+                            f.write("\n")
+                        else:
+                            f.write(intro_nvt + nvt + "\n")
+                            f.write("\n")
+                            f.write(f"{mensaje}\n")
+                            f.write("\n")
+        f.close()
+    print(f"Los resultados se han guardado en el archivo '{output_file}'.")
+    with open(ai_info, "w") as f:
+        for line in uniq_nvt:
+            if len(hosts) > 0:
+                f.write(f"{line}\n")
+                f.write("\n")
